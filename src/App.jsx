ac84f55e66e878ceacdc19df6162884ffb66a771
filbert6431 <filbert24si@mahasiswa.pcr.ac.id>
2026-04-21 10:15:29 +0700

    pertemuan 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Dashboard from "./pages/Dashboard";
+import Header from "./layouts/header";
+import Sidebar from "./layouts/sidebar";
+import { Link, Route, Routes } from "react-router-dom"
+import './assets/tailwind.css';
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Orders from './pages/orders';
+import Customers from './pages/Customers';
+import NotFound from './pages/NotFound';
+// import reactLogo from './assets/react.svg' enggak dipakai
+// import viteLogo from '/vite.svg' enggak dipakai
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0) enggak dipakai
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="app-container" className="bg-pink-200 min-h-screen flex flex-col">
+
+      <div id="layout-wrapper" className="flex flex-row flex-1">
+        <Sidebar />
+        <div id="main-content" className="flex-1 p-4">
+          <Header />
+
+          {/* <Dashboard /> */}
+           <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/Customers" element={<Customers />} />
+
+            {/* Kalau kita klik selain yang diatas, dia akan menampilkan halaman notFound */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
-      <h1> Fikri PCR</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/pages/Customers.jsx
@@ -0,0 +1,12 @@
+import PageHeader from "../components/PageHeader";
+
+export default function Customers() {
+    return (
+        <div >
+            <PageHeader />
+
+            <h1> Ini alaman customer</h1>
+
+        </div>
+    );
+}
