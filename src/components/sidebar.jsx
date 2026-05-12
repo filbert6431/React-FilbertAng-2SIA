d56e0e083bbb8d26013798f9f04aa8df0756d905
filbert6431 <filbert24si@mahasiswa.pcr.ac.id>
2026-05-12 10:19:27 +0700

    pertemuan(-fixSidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,5 @@
 import { BsPeople } from "react-icons/bs";
-import { TbDashboardOff } from "react-icons/tb";
+import { MdDashboard } from "react-icons/md";
 import { FaClipboardList } from "react-icons/fa";
 import { HiAcademicCap } from "react-icons/hi";
 import { Link, NavLink } from "react-router-dom";
@@ -36,7 +36,7 @@ export default function Sidebar() {
                                 to="/">
                                 {/* //to adalah element untuk pindah halaman, dalam kasus ini ia pindah ke dashboard
                                 //   karena sudah kita set kalau " / " dia pergi ke dashboard, di halaman amain */}
-                                <TbDashboardOff className="text-xl mr-2 " />
+                                <MdDashboard className="text-xl mr-2 " />
                                 Dashboard
                             </NavLink>
                         </li>
